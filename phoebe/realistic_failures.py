2f9c2573160cea8e9fc24440136bc1d37ee88e84
GluckZhang <zhanglong3030@qq.com>
2020-03-21 01:19:34 +0100

    change the query since the monitor exports failure rate now

--- phoebe/realistic_failures.py
+++ phoebe/realistic_failures.py
@@ -9,7 +9,6 @@ import logging
 PROMETHEUS_URL = ''
 QUERY_API = '/api/v1/query'
 RANGE_QUERY_API = '/api/v1/query_range'
-OUTPUTFILE = '' # default: result.csv
 START = '' # rfc3339 | unix_timestamp
 END = '' # rfc3339 | unix_timestamp
 STEP = ''
@@ -18,15 +17,11 @@ PERIOD = 60 # unit: miniute, default 60
 def main():
     handle_args(sys.argv[1:])
 
-    failure_category = query_failure_category(START, END, STEP)
-    failure_details = query_failure_detail(failure_category, START, END, STEP)
-    failure_details = calculate_failure_rate(failure_details, STEP)
-
-    pretty_print_details(failure_details)
+    failure_category = query_failures(START, END, STEP)
+    pretty_print_details(failure_category)
 
 def handle_args(argv):
     global PROMETHEUS_URL
-    global OUTPUTFILE
     global START
     global END
     global STEP
@@ -45,8 +40,6 @@ def handle_args(argv):
             sys.exit()
         elif opt in ("-h", "--host"):
             PROMETHEUS_URL = arg
-        elif opt in ("-o", "--outfile"):
-            OUTPUTFILE = arg
         elif opt in ("-s", "--step"):
             STEP = arg
         elif opt == "--start":
@@ -61,15 +54,11 @@ def handle_args(argv):
         print_help_info()
         sys.exit(2)
 
-    if OUTPUTFILE == '':
-        OUTPUTFILE = 'result.csv'
-        logging.warning("You didn't specify output file's name, will use default name %s", OUTPUTFILE)
     if STEP == '':
         STEP = '15s'
         logging.warning("You didn't specify query resolution step width, will use default value %s", STEP)
-    if PERIOD == '' and START == '' and END == '':
-        PERIOD = 10
-        logging.warning("You didn't specify query period or start&end time, will query the latest %s miniutes' data as a test", PERIOD)
+    if START == '' and END == '':
+        logging.warning("You didn't specify start&end time, will query the latest %s miniutes' data as a test", PERIOD)
 
 def print_help_info():
     print('')
@@ -80,10 +69,10 @@ def print_help_info():
     print('Additional options: --start=<start_timestamp_or_rfc3339> --end=<end_timestamp_or_rfc3339> --period=<get_for_most_recent_period(int miniutes)>')
     print('                    use start&end or only use period')
 
-def query_failure_category(start_time, end_time, step):
+def query_failures(start_time, end_time, step):
     failure_category = list()
 
-    query_string = 'increase(failed_syscalls_total{error_code!="SUCCESS"}[%s])'%step
+    query_string = 'syscalls_failure_rate'
     response = requests.post(PROMETHEUS_URL + RANGE_QUERY_API, data={'query': query_string, 'start': start_time, 'end': end_time, 'step': step})
     status = response.json()["status"]
 
@@ -94,60 +83,25 @@ def query_failure_category(start_time, end_time, step):
     results = response.json()['data']['result']
 
     for entry in results:
+        if len(entry["values"]) == 1:
+            samples = [{"timestamp": entry["values"][0][0], "failure_rate": float(entry["values"][0][1])}] # only one case
+        else:
+            samples = [
+                {"timestamp": entry["values"][0][0], "failure_rate": float(entry["values"][0][1])}, # first case
+                {"timestamp": entry["values"][-1][0], "failure_rate": float(entry["values"][-1][1])} # last case
+            ]
         failure_category.append({
             "syscall_name": entry["metric"]["syscall_name"],
-            "error_code": entry["metric"]["error_code"]
+            "error_code": entry["metric"]["error_code"],
+            "samples_in_total": len(entry["values"]),
+            "samples": samples
         })
     
     return failure_category
 
-def query_failure_detail(failure_category, start_time, end_time, step):
-    failure_details = list()
-
-    query_string = 'increase(failed_syscalls_total{error_code!="SUCCESS",syscall_name="%s"}[%s])>0'
-    for category in failure_category:
-        response = requests.post(PROMETHEUS_URL + RANGE_QUERY_API, data={'query': query_string%(category["syscall_name"], step), 'start': start_time, 'end': end_time, 'step': step})
-        status = response.json()["status"]
-
-        if status == "error":
-            logging.error(response.json())
-            sys.exit(2)
-        
-        results = filter(lambda x: len(x) > 0, response.json()['data']['result'])
-
-        for entry in results:
-            failure_details.append({
-                "syscall_name": entry["metric"]["syscall_name"],
-                "error_code": entry["metric"]["error_code"],
-                "cases_in_total": len(entry["values"]),
-                "samples": [
-                    {"timestamp": entry["values"][0][0], "failures_count": entry["values"][0][1]}, # first case
-                    {"timestamp": entry["values"][-1][0], "failures_count": entry["values"][-1][1]} # last case
-                ]
-            })
-
-    return failure_details
-
-def calculate_failure_rate(failure_details, step):
-    query_string = 'sum without(error_code)(increase(failed_syscalls_total{syscall_name="%s"}[%s]))'
-
-    for detail in failure_details:
-        for sample in detail["samples"]:
-            response = requests.post(PROMETHEUS_URL + QUERY_API, data={'query': query_string%(detail["syscall_name"], step), 'time': sample["timestamp"], 'step': step})
-            status = response.json()["status"]
-
-            if status == "error":
-                logging.error(response.json())
-                continue
-        
-            sample["total_count"] = float(response.json()["data"]["result"][0]["value"][1])
-            sample["failure_rate"] = float(sample["failures_count"]) / sample["total_count"]
-
-    return failure_details
-
 def pretty_print_details(failure_details):
     stat_table = PrettyTable()
-    stat_table.field_names = ["Syscall Name", "Error Code", "Cases in Total", "Samples"]
+    stat_table.field_names = ["Syscall Name", "Error Code", "Samples in Total", "Samples"]
 
     for detail in failure_details:
         samples_str = ""
@@ -155,7 +109,7 @@ def pretty_print_details(failure_details):
             localtime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(sample["timestamp"]))
             samples_str += "localtime: %s, failure rate: %2f\n"%(localtime, sample["failure_rate"])
         samples_str = samples_str[:-1]
-        stat_table.add_row([detail["syscall_name"], detail["error_code"], detail["cases_in_total"], samples_str])
+        stat_table.add_row([detail["syscall_name"], detail["error_code"], detail["samples_in_total"], samples_str])
 
     print(stat_table)
 
